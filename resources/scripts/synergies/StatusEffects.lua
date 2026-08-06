05fa7035c2583d35c359663b531d01b089d52e8a
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-08-05 22:04:13 -0700

    Improved Status effect stomp/parry synergies

--- resources/scripts/synergies/StatusEffects.lua
+++ resources/scripts/synergies/StatusEffects.lua
@@ -1,13 +1,15 @@
 local mod = EdithRebuilt
 local Callbacks = mod.Enums.Callbacks
+local bitMask = mod.Modules.BIT_MASK
 
 ---@param player EntityPlayer
 ---@param entity Entity
 local function ApplyTearflagEffects(player, entity)
 	if not entity:IsEnemy() then return end
 
+	local flag = player:GetTearHitParams(WeaponType.WEAPON_BRIMSTONE).TearFlags
 	local ent = entity:ToNPC() ---@cast ent EntityNPC
-	ent:ApplyTearflagEffects(ent.Position, player.TearFlags, player, player.Damage)
+	ent:ApplyTearflagEffects(ent.Position, flag, player, player.Damage)
 end
 
 mod:AddCallback(Callbacks.PERFECT_PARRY, function(_, player, entity) ApplyTearflagEffects(player, entity) end)
