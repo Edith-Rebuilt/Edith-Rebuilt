70cb83e4889031363861f00c00758ab787c166c9
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-08-15 10:22:36 -0700

    Fixed Edith/T. Edith's Greedier unlock function triggering for other players

--- resources/scripts/misc/UnlockManager.lua
+++ resources/scripts/misc/UnlockManager.lua
@@ -119,6 +119,8 @@ local function GreedierUnlockManager(player, difficulty)
 
     local unlockTable = Helpers.When(player, GreedierUnlocks)
 
+    if not unlockTable then return end
+
     for _, ach in ipairs(unlockTable) do
         pgd:TryUnlock(ach)
     end
