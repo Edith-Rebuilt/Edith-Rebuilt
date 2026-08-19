abca428849a394f845e7ed9833bb72c88beafa75
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-08-18 19:13:14 -0700

    Update Helpers.lua

--- resources/scripts/functions/Helpers.lua
+++ resources/scripts/functions/Helpers.lua
@@ -75,7 +75,7 @@ function Helpers.DestroyGrid(entity, radius)
 		if (entity.Position - grid.Position):Length() > radius then goto continue end
 		if grid:GetType() == GridEntityType.GRID_DOOR then goto continue end
 
-		grid:DestroyWithSource(true, EntityRef(entity))
+		grid:DestroyWithSource(false, EntityRef(entity))
 
 		::continue::
     end
