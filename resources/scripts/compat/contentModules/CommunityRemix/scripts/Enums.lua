8770836916b837d1fdf55507df48664b0dbc9c1e
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-08-23 16:57:40 -0700

    Added 1/3 and 2/3 salt hearts

--- resources/scripts/compat/contentModules/CommunityRemix/scripts/Enums.lua
+++ resources/scripts/compat/contentModules/CommunityRemix/scripts/Enums.lua
@@ -2,7 +2,9 @@ local mainEnums = EdithRebuilt.Enums
 
 EdithRebuilt_SaltHearts.Enums = {
     HeartSubType = {
-        SALT_HEART = Isaac.GetEntitySubTypeByName("Salt Heart (pickup)")
+        SALT_HEART = Isaac.GetEntitySubTypeByName("Salt Heart (pickup)"),
+        SALT_HEART_2_THIRDS = Isaac.GetEntitySubTypeByName("Salt Heart (pickup 2/3)"),
+        SALT_HEART_1_THIRD = Isaac.GetEntitySubTypeByName("Salt Heart (pickup 1/3)"),
     },
     Achievement = {
         SALT_HEART = Isaac.GetAchievementIdByName("Salt Hearts"),
--- resources/scripts/compat/contentModules/CommunityRemix/scripts/Health.lua
+++ resources/scripts/compat/contentModules/CommunityRemix/scripts/Health.lua
@@ -4,7 +4,7 @@ if not CustomHealthAPI.Library then return end
 local mod = EdithRebuilt
 local mainEnums = mod.Enums
 local enums = EdithRebuilt_SaltHearts.Enums
-local SubType = Isaac.GetEntitySubTypeByName("Salt Heart (pickup)")
+local SubTypes = enums.HeartSubType
 local sounds = mainEnums.SoundEffect
 local saveManager = mod.SaveManager
 local StatusEffect = mod.Modules.STATUS_EFFECTS
@@ -26,7 +26,21 @@ CustomHealthAPI.Library.RegisterSoulHealth(
         CollectSound = { ID = sounds.SOUND_SALT_SHAKER, Volume = 1.0, Pitch = 1.0 },
 	    PrioritizeHealing = true,
         PickupEntities = {
-            {ID = EntityType.ENTITY_PICKUP, Var = PickupVariant.PICKUP_HEART, Sub = enums.HeartSubType.SALT_HEART}
+            {
+                ID = EntityType.ENTITY_PICKUP,
+                Var = PickupVariant.PICKUP_HEART,
+                Sub = SubTypes.SALT_HEART
+            },
+            {
+                ID = EntityType.ENTITY_PICKUP, 
+                Var = PickupVariant.PICKUP_HEART, 
+                Sub = SubTypes.SALT_HEART_2_THIRDS
+            },
+            {
+                ID = EntityType.ENTITY_PICKUP, 
+                Var = PickupVariant.PICKUP_HEART, 
+                Sub = SubTypes.SALT_HEART_1_THIRD
+            },
         },
         SumptoriumSubType = 20,  -- immortal heart clot
         SumptoriumSplatColor = Color(1.00, 1.00, 1.00, 1.00, 214/255, 228/255, 1.00),
@@ -42,14 +56,23 @@ CustomHealthAPI.Library.RegisterSoulHealth(
     }
 )
 
-CustomHealthAPI.Library.RegisterHeartPickup(PickupVariant.PICKUP_HEART, enums.HeartSubType.SALT_HEART, {
-	HealthKeys = {"HEART_SALT"},
-	HealthAmount = 3,
-	DropSound = sounds.SOUND_SALT_SHAKER,
-	AllowCandyHeartSoulLocketBonus = true,
-	AllowImmaculateConception = false,
-    AllowMagneto = true,
-})
+local health = {
+    [SubTypes.SALT_HEART] = 3,
+    [SubTypes.SALT_HEART_2_THIRDS] = 2,
+    [SubTypes.SALT_HEART_1_THIRD] = 1
+}
+
+for heart, amount in pairs(health) do
+    CustomHealthAPI.Library.RegisterHeartPickup(PickupVariant.PICKUP_HEART, heart, {
+        HealthKeys = {"HEART_SALT"},
+        HealthAmount = amount,
+        DropSound = sounds.SOUND_SALT_SHAKER,
+        AllowCandyHeartSoulLocketBonus = true,
+        AllowImmaculateConception = false,
+        AllowMagneto = true,
+    })
+end
+
 
 CustomHealthAPI.Library.AddCallback("EdithRebuilt", CustomHealthAPI.Enums.Callbacks.POST_HEALTH_DAMAGED, 0, function(player, flags, key, hpDamaged, wasDepleted, wasLastDamaged)
     if key ~= "HEART_SALT" then return end
