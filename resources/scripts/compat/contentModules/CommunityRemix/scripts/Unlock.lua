f495ff880250d3fbd852edf25bfed170e8e10eb5
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-08-23 06:49:29 -0700

    aaaa

--- resources/scripts/compat/contentModules/CommunityRemix/scripts/Unlock.lua
+++ resources/scripts/compat/contentModules/CommunityRemix/scripts/Unlock.lua
@@ -1,11 +1,10 @@
 local mod = EdithRebuilt
 local Player = mod.Modules.PLAYER
 
-local Achievement = Isaac.GetAchievementIdByName("Salt Hearts")
 
-mod:AddCallback(ModCallbacks.MC_POST_COMPLETION_EVENT, function(_, mark)
+EdithRebuilt_SaltHearts:AddCallback(ModCallbacks.MC_POST_COMPLETION_EVENT, function(_, mark)
     if mark ~= "InsaneMode" then return end
     if not Player.AnyoneIsEdith() then return end
 
-    mod.Enums.Utils.PGD:TryUnlock(Achievement)
+    mod.Enums.Utils.PGD:TryUnlock(EdithRebuilt_SaltHearts.Enums.Achievement.SALT_HEART)
 end)
