cf3686503e093192c54176ddb4b02906a51c391a
AutisticGoat <83523192+AutisticGoat@users.noreply.github.com>
2026-09-17 21:50:57 -0700

    Updated T. Edith's description, added proper names to Birthright's descriptions

--- resources/scripts/compat/EID.lua
+++ resources/scripts/compat/EID.lua
@@ -46,16 +46,17 @@ ERSalt
 EID:addCharacterInfo(PlayerType.PLAYER_EDITH_B, [[Unable to walk, spawns an arrow instead
 Trying to walk will charge a chargebar, the more charge the faster she'll move in the arrow's direction
 Pressing {{ButtonRT}} will make her jump in place, performing a parry, its effect depends on enemy's distance
-A Perfect Parry damage the enemy, increases the dash charge and gives 20 i-frames
+A Perfect Parry damage the enemy, increases the dash charge, gives 20 i-frames, and overheats Edith
+If Edith is overheat, both damage and cooldown of the parry will be increased, heat will be lowered over time
 ]], "Tainted Edith")
 
-EID:addBirthright(PlayerType.PLAYER_EDITH, "Edith's Stomps will have increased damage# Stomp synergies will be powered")
+EID:addBirthright(PlayerType.PLAYER_EDITH, "Edith's Stomps will have increased damage# Stomp synergies will be powered", "Edith")
 EID:addBirthright(PlayerType.PLAYER_EDITH_B, [[
 Hopdash charge can be overcharged to 200%
 Overcharged hopdash can apply burn status effect
 Cinder created by perfect parrying enemies will spawn fire jets below them
 Parried projectiles and tears will have fire effects
-]])
+]], "Tainted Edith")
 
 local SpicesColors = {
     ["ColorSalted"] = KColor(1, 1, 1, 1),
